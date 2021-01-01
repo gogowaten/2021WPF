987e8f447f9d7c2462368368626fb83be7791cf2
waten <gogowaten@yahoo.co.jp>
2021-01-01 14:59:09 +0900

    202101011459

---
+++ 20210101_GetWindow/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Interop;
+
+namespace _20210101_GetWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ホットキー
+        private const int HOTKEY_ID1 = 0x0001;//ID
+        private IntPtr MyWindowHandle;//アプリのハンドル
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyInitializeHotKey();
+            //ホットキー登録
+            ChangeHotKey(Key.PrintScreen, HOTKEY_ID1);
+
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //ホットキーの登録解除
+            _ = API.UnregisterHotKey(MyWindowHandle, HOTKEY_ID1);
+            ComponentDispatcher.ThreadPreprocessMessage -= ComponentDispatcher_ThreadPreprocessMessage;
+        }
+
+        private IntPtr GetParentWindowFromForegroundWindow()
+        {
+            //最前面ウィンドウを起点にWindowTextがあるもの(GetWindowTextの戻り値が0以外)をGetParentで10回まで辿る            
+            //見つからなかった場合は最前面ウィンドウのハンドルにする
+            IntPtr hForeWnd = API.GetForegroundWindow();
+            var wndText = new StringBuilder(65535);
+            int count = 0;
+            IntPtr hWnd = hForeWnd;
+            while (API.GetWindowText(hWnd, wndText, 65535) == 0)
+            {
+                hWnd = API.GetParent(hWnd);
+                count++;
+                if (count > 10)
+                {
+                    hWnd = hForeWnd;
+                    break;
+                }
+            }
+            return hWnd;
+        }
+
+        //ホットキー動作
+        private void ComponentDispatcher_ThreadPreprocessMessage(ref MSG msg, ref bool handled)
+        {
+            if (msg.message != API.WM_HOTKEY) return;
+            else if (msg.wParam.ToInt32() == HOTKEY_ID1)
+            {
+                //MessageBox.Show("ホットキーーーーーーーーーーーー");
+
+            }
+        }
+        private void MyInitializeHotKey()
+        {
+            MyWindowHandle = new WindowInteropHelper(this).Handle;
+            ComponentDispatcher.ThreadPreprocessMessage += ComponentDispatcher_ThreadPreprocessMessage;
+        }
+        private void ChangeHotKey(Key Key, int hotkeyId)
+        {
+            ChangeHotKey(KeyInterop.VirtualKeyFromKey(Key), hotkeyId);
+        }
+        private void ChangeHotKey(int vKey, int hotkeyId)
+        {
+            //上書きはできないので、古いのを削除してから登録
+            _ = API.UnregisterHotKey(MyWindowHandle, hotkeyId);
+
+            //int mod = GetModifierKeySum();
+            int mod = 0;
+            if (API.RegisterHotKey(MyWindowHandle, hotkeyId, mod, vKey) == 0)
+            {
+                MessageBox.Show("登録に失敗");
+                
+                //MyGroupBoxHotKey.Header = "無効なホットキー";
+            }
+            else
+            {
+                //MessageBox.Show("登録完了");
+                
+                //MyGroupBoxHotKey.Header = "ホットキー";
+            }
+        }
+
+        //private int GetModifierKeySum()
+        //{
+        //    int mod = 0;
+        //    if (MyAppConfig.HotkeyAlt) mod += (int)ModifierKeys.Alt;
+        //    if (MyAppConfig.HotkeyCtrl) mod += (int)ModifierKeys.Control;
+        //    if (MyAppConfig.HotkeyShift) mod += (int)ModifierKeys.Shift;
+        //    if (MyAppConfig.HotkeyWin) mod += (int)ModifierKeys.Windows;
+        //    return mod;
+        //}
+
+    }
+
+    public static class API
+    {
+        //Rect取得用
+        public struct RECT
+        {
+            //型はlongじゃなくてintが正解！！！！！！！！！！！！！！
+            //longだとおかしな値になる
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public override string ToString()
+            {
+                return $"横:{right - left:0000}, 縦:{bottom - top:0000}  ({left}, {top}, {right}, {bottom})";
+            }
+        }
+        //座標取得用
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        //ウィンドウのRect取得
+        [DllImport("user32.dll")]
+        internal static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        //手前にあるウィンドウのハンドル取得
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetForegroundWindow();
+
+        //ウィンドウ名取得
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        internal static extern int GetWindowText(IntPtr hWin, StringBuilder lpString, int nMaxCount);
+
+        //パレントウィンドウ取得
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetParent(IntPtr hWnd);
+
+
+        //グローバルホットキー登録用
+        internal const int WM_HOTKEY = 0x0312;
+        [DllImport("user32.dll")]
+        internal static extern int RegisterHotKey(IntPtr hWnd, int id, int modkyey, int vKey);
+        [DllImport("user32.dll")]
+        internal static extern int UnregisterHotKey(IntPtr hWnd, int id);
+
+    }
+}
