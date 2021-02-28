d71ed91be4926d46de0e1883990ea6f6e2615b47
waten <gogowaten@yahoo.co.jp>
2021-02-28 16:25:34 +0900

    202102281625

--- 20210227_thumb移動入れ替え/20210227_thumb移動入れ替え/MainWindow.xaml.cs
+++ 20210227_thumb移動入れ替え/20210227_thumb移動入れ替え/MainWindow.xaml.cs
@@ -15,6 +15,11 @@ using System.Windows.Shapes;
 using System.Windows.Controls.Primitives;
 using System.Collections.ObjectModel;
 
+
+//左上から右下へ敷き詰められた3x3個のThumb
+//マウスドラッグ移動で移動先のThumbと入れ替える感じの動き
+//移動先にあるThumbから後ろのThumbすべてを1個づつ右下へ移動させる
+
 namespace _20210227_thumb移動入れ替え
 {
     /// <summary>
@@ -23,6 +28,7 @@ namespace _20210227_thumb移動入れ替え
     public partial class MainWindow : Window
     {
         private ObservableCollection<FlatThumb> MyThumbs;
+        //ドラッグ移動中のThumbのIndexに対応するRectと、ThumbのRectの重なり合う面積保持用
         private double moto重なり面積;
         public MainWindow()
         {
@@ -49,33 +55,42 @@ namespace _20210227_thumb移動入れ替え
             }
         }
 
+        //ドラッグ移動終了時、最寄りというか空いている(あるべき)場所に移動させる
         private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             FlatThumb t = sender as FlatThumb;
-            Idoutyuu(t);
-            var p = MyCanvas.PointToScreen(new Point());
-            RectangleGeometry tg = GetGeometry(t, p);
-            int left = (int)((tg.Bounds.Left / 100.0) + 0.5) * 100;
-            int top = (int)((tg.Bounds.Top / 100.0) + 0.5) * 100;
-            Canvas.SetLeft(t, left);
-            Canvas.SetTop(t, top);
+            int imaIndex = MyThumbs.IndexOf(t);
+            int x = imaIndex % 3 * 100;//元のx座標
+            int y = imaIndex / 3 * 100;
+            Canvas.SetLeft(t, x);
+            Canvas.SetTop(t, y);
+
             MyStatus.Content = "";
 
         }
 
+        /// <summary>
+        /// ドラッグ移動中のThumbとその他のThumbとの重なり合う部分の面積を計算、
+        /// 一定以上の面積があった場合、場所を入れ替えて整列
+        /// </summary>
+        /// <param name="t">ドラッグ移動中のThumb</param>
         private void Idoutyuu(FlatThumb t)
         {
             t.Opacity = 0.7;
             var p = MyCanvas.PointToScreen(new Point());
-            RectangleGeometry tg = GetGeometry(t, p);
-            List<double> kasanai面積 = new();
-            int tID = MyThumbs.IndexOf(t);//ドラッグ移動中ThumbのIndex
-            MyStatus.Content = tID.ToString();
+            RectangleGeometry geometry = GetThumbGeometry(t, p);
+            int imaIndex = MyThumbs.IndexOf(t);//ドラッグ移動中ThumbのIndex
+            MyStatus.Content = imaIndex.ToString();
 
             //重なり面積のリスト作成
+            List<double> kasanai面積 = new();
             for (int i = 0; i < MyThumbs.Count; i++)
             {
-                Rect bound = Geometry.Combine(tg, GetGeometry(MyThumbs[i], p), GeometryCombineMode.Intersect, null).Bounds;
+                Rect bound = Geometry.Combine(
+                    geometry,
+                    GetThumbGeometry(MyThumbs[i], p),
+                    GeometryCombineMode.Intersect, null).Bounds;
+
                 if (bound == Rect.Empty)
                 {
                     kasanai面積.Add(0);
@@ -86,38 +101,49 @@ namespace _20210227_thumb移動入れ替え
                 }
             }
 
-            //重なり面積が4000以上のThumbがあれば、移動中Thumbと入れ替える
-            int saki = 0;
+            int sakiIndex = 0;//移動先のIndex
             double max = 0;
             for (int i = 0; i < kasanai面積.Count; i++)
             {
-                if (max < kasanai面積[i] && i != tID)
+                if (max < kasanai面積[i] && i != imaIndex)
                 {
                     max = kasanai面積[i];
-                    saki = i;
+                    sakiIndex = i;
                 }
             }
 
-            //元の位置との重なり面積
-            int x = tID % 3 * 100;
-            int y = tID / 3 * 100;
-            var motoRect = new RectangleGeometry(new Rect(new Point(x, y), new Size(100, 100)));
-            var moto重なりbound = Geometry.Combine(motoRect, tg, GeometryCombineMode.Intersect, null).Bounds;
-            var moto重なり面積temp = moto重なりbound.Width * moto重なりbound.Height;
+            //移動中ThumbのIndexに対応するRectと、移動中ThumbのRect重なり面積
+            int x = imaIndex % 3 * 100;//元のx座標
+            int y = imaIndex / 3 * 100;
+            var idxGeo = new RectangleGeometry(new Rect(new Point(x, y), new Size(100, 100)));
+            var ima重なりbound = Geometry.Combine(idxGeo, geometry, GeometryCombineMode.Intersect, null).Bounds;
+            var ima重なり面積 = ima重なりbound.Width * ima重なりbound.Height;
 
-            if (max > 4000 && moto重なり面積 > moto重なり面積temp)
+            //重なり面積が4000以上のThumbがある
+            //and 重なり面積が
+            //移動中Thumbと入れ替える
+            if (max > 4000 && moto重なり面積 > ima重なり面積)
             {
-                Idou(saki, tID, t, tg);
+                //Thumbのindexと場所並べ替え
+                SortIndexPlace(sakiIndex, imaIndex, t, geometry);
             }
             else
             {
-                moto重なり面積 = moto重なり面積temp;
+                moto重なり面積 = ima重なり面積;
             }
         }
 
-        //Thumbの場所入れ替え
-        private void Idou(int saki, int tID, FlatThumb t, RectangleGeometry tg)
+        //
+        /// <summary>
+        /// Thumbのindexと場所並べ替え
+        /// </summary>
+        /// <param name="saki">移動中Thumbの新しいindex</param>
+        /// <param name="tID">移動中Thumbのindex</param>
+        /// <param name="t">移動中Thumb</param>
+        /// <param name="tg">移動中ThumbのRectangleGeometry</param>
+        private void SortIndexPlace(int saki, int tID, FlatThumb t, RectangleGeometry tg)
         {
+            //今のindexより大きくなる場合は、そのindexより後ろのThumbを並べ替える
             if (saki > tID)
             {
                 for (int tt = tID + 1; tt <= saki; tt++)
@@ -125,12 +151,15 @@ namespace _20210227_thumb移動入れ替え
                     FlatThumb target = MyThumbs[tt];
                     double left = Canvas.GetLeft(target);
                     double top = Canvas.GetTop(target);
-                    if (left >= 100)
+                    //左端以外なら
+                    if (left != 0)
                     {
+                        //左へ移動
                         Canvas.SetLeft(target, left - 100);
                     }
                     else
                     {
+                        //一段上の右端へ移動
                         if (top >= 100)
                         {
                             Canvas.SetTop(target, top - 100);
@@ -138,8 +167,7 @@ namespace _20210227_thumb移動入れ替え
                         }
                     }
                 }
-                MyThumbs.RemoveAt(tID);
-                MyThumbs.Insert(saki, t);
+                
             }
             else
             {
@@ -161,24 +189,32 @@ namespace _20210227_thumb移動入れ替え
                         }
                     }
                 }
-                MyThumbs.RemoveAt(tID);
-                MyThumbs.Insert(saki, t);
+                
             }
+            //index変更
+            MyThumbs.RemoveAt(tID);
+            MyThumbs.Insert(saki, t);
 
-            //元の位置との重なり面積
+            //移動中Thumbのindexが変更されたので、対応するRectと今のThumbのRectとの重なり面積を再計算
             int x = saki % 3 * 100;
             int y = saki / 3 * 100;
-            var motoRect = new RectangleGeometry(new Rect(new Point(x, y), new Size(100, 100)));
-            var moto重なりbound = Geometry.Combine(motoRect, tg, GeometryCombineMode.Intersect, null).Bounds;
-            moto重なり面積 = moto重なりbound.Width * moto重なりbound.Height;
+            var imaRect = new RectangleGeometry(new Rect(new Point(x, y), new Size(100, 100)));
+            var ima重なりbound = Geometry.Combine(imaRect, tg, GeometryCombineMode.Intersect, null).Bounds;
+            moto重なり面積 = ima重なりbound.Width * ima重なりbound.Height;
 
         }
 
-        private RectangleGeometry GetGeometry(FlatThumb t, Point p)
+        //
+        /// <summary>
+        /// ThumbのRectanleGeometry作成
+        /// </summary>
+        /// <param name="t">Thumb</param>
+        /// <param name="p">Thumbの親パネルの座標</param>
+        /// <returns></returns>
+        private RectangleGeometry GetThumbGeometry(FlatThumb t, Point p)
         {
-            Rect tr = new((Point)Point.Subtract(t.PointToScreen(new Point()), p), new Size(100, 100));
-            RectangleGeometry tg = new(tr);
-            return tg;
+            //親パネル上での座標なので、Thumbと親パネル座標で引き算Subtract
+            return new(new Rect((Point)Point.Subtract(t.PointToScreen(new Point()), p), new Size(100, 100)));
         }
 
 
@@ -196,7 +232,7 @@ namespace _20210227_thumb移動入れ替え
     public class FlatThumb : Thumb
     {
         private Grid MyPanel;
-        private static int ElementCount;
+        
         public FlatThumb(string text)
         {
             ControlTemplate template = new(typeof(Thumb));
@@ -204,7 +240,7 @@ namespace _20210227_thumb移動入れ替え
             this.Template = template;
             this.ApplyTemplate();
             MyPanel = (Grid)template.FindName("panel", this);
-            MyPanel.Background = Brushes.MediumAquamarine;
+            MyPanel.Background = Brushes.Aqua;
 
             MyPanel.Children.Add(new TextBlock()
             {
@@ -212,7 +248,7 @@ namespace _20210227_thumb移動入れ替え
                 TextAlignment = TextAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center
             });
-            ElementCount++;
+            MyPanel.Children.Add(new Border() { BorderBrush = Brushes.MediumBlue, BorderThickness=new Thickness(1) });
         }
     }
 }
