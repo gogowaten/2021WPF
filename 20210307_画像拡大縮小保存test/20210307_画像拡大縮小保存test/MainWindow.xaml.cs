a33d8bc15e5b786b09da41a750b184fed6c93130
waten <gogowaten@yahoo.co.jp>
2021-03-08 08:46:21 +0900

    202103080846

---
+++ 20210307_画像拡大縮小保存test/20210307_画像拡大縮小保存test/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20210307_画像拡大縮小保存test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyInitialize()
+        {
+            string path = @"D:\ブログ用\チェック用2\WP_20210228_11_25_51_Pro_2021_02_28_午後わてん.jpg";
+            BitmapImage bitmapImage = new BitmapImage(new Uri(path));
+            DrawingImage di = new DrawingImage();
+            
+        }
+    }
+}
