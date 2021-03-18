b82ec62c2ad00ea9f6430489cc72caea2c0312fa
waten <gogowaten@yahoo.co.jp>
2021-03-18 18:19:30 +0900

    202103181819

---
+++ 20210318_画像連結テスト/20210318_画像連結テスト/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20210318_画像連結テスト
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Border> MyBorders = new();
+        private List<Image> MyImages = new();
+        private double MyImageSize = 200;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop) == false) return;
+            var datas = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var paths = datas.ToList();
+            paths.Sort();
+
+            for (int i = 0; i < paths.Count; i++)
+            {
+                BitmapSource source = MakeBitmapSource(paths[i]);
+                Image img = new()
+                {
+                    Source = source,
+                    Stretch = Stretch.Uniform,
+                    Width = MyImageSize,
+                    Height = MyImageSize,
+                };
+                source.Freeze();
+                MyImages.Add(img);
+                MyPanel.Children.Add(img);
+
+            }
+            
+        }
+
+
+        private BitmapSource MakeBitmapSource(string filePath, double dpiX = 96, double dpiY = 96)
+        {
+            BitmapSource source = null;
+            try
+            {
+                using (var stream = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    var frame = BitmapFrame.Create(stream);
+                    int w = frame.PixelWidth;
+                    int h = frame.PixelHeight;
+                    int stride = (w * frame.Format.BitsPerPixel + 7) / 8;
+                    var pixels = new byte[h * stride];
+                    frame.CopyPixels(pixels, stride, 0);
+                    source = BitmapSource.Create(w, h, dpiX, dpiY, frame.Format, frame.Palette, pixels, stride);
+                };
+            }
+            catch (Exception)
+            {
+
+            }
+            return source;
+        }
+    }
+}
