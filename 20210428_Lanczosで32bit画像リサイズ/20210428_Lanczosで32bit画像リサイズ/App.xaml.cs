8751990433ddbe7d95471cb4132853f8b4e645cd
waten <gogowaten@yahoo.co.jp>
2021-04-28 10:53:15 +0900

    202104281053

---
+++ 20210428_Lanczosで32bit画像リサイズ/20210428_Lanczosで32bit画像リサイズ/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _20210428_Lanczosで32bit画像リサイズ
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
