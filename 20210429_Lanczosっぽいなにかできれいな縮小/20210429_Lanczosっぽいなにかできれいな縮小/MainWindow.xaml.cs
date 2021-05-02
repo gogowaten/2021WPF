659c631ca199d91bd9edb878e1ae6c91303005d3
waten <gogowaten@yahoo.co.jp>
2021-05-03 00:49:45 +0900

    202105030049

--- 20210429_Lanczosっぽいなにかできれいな縮小/20210429_Lanczosっぽいなにかできれいな縮小/MainWindow.xaml.cs
+++ 20210429_Lanczosっぽいなにかできれいな縮小/20210429_Lanczosっぽいなにかできれいな縮小/MainWindow.xaml.cs
@@ -64,7 +64,21 @@ namespace _20210429_Lanczosっぽいなにかできれいな縮小
         {
             var sw = new Stopwatch();
             sw.Start();
-            var bitmap = func(source, width, height, n, weightFunc);
+            BitmapSource bitmap = func(source, width, height, n, weightFunc);
+            sw.Stop();
+            MyStatusItem.Content = $"処理時間：{sw.Elapsed.TotalSeconds:000.000}秒, {func.Method.Name}, {weightFunc.Method.Name}";
+            MyImage.Source = bitmap;
+        }
+        //処理時間計測
+        private void MyExe2(
+            Func<BitmapSource, int, int, int, double, Func<double, int, double, double>, BitmapSource> func,
+            Func<double, int, double, double> weightFunc,
+            BitmapSource source, int width, int height, int n, double scale)
+
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            BitmapSource bitmap = func(source, width, height, n, scale, weightFunc);
             sw.Stop();
             MyStatusItem.Content = $"処理時間：{sw.Elapsed.TotalSeconds:000.000}秒, {func.Method.Name}, {weightFunc.Method.Name}";
             MyImage.Source = bitmap;
@@ -259,6 +273,97 @@ namespace _20210429_Lanczosっぽいなにかできれいな縮小
             else
             {
                 return Sinc(d / (n - 0.5)) * Sinc(d / n);
+            }
+        }
+        private double GetLanczosWeightR(double d, int n)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d) * Sinc(d / (n));
+            }
+        }
+        private double GetLanczosWeightS(double d, int n)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / 2.0) * Sinc(d / (n * 2));
+            }
+        }
+        private double GetLanczosWeightT(double d, int n)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / (n * 0.75)) * Sinc(d / n);
+            }
+        }
+        private double GetLanczosWeightU(double d, int n)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / (n * 0.75)) * Sinc(d / (n * n));
+            }
+        }
+        private double GetLanczosWeightV(double d, int n)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / (n * 0.5)) * Sinc(d / n);
+            }
+        }
+        private double GetLanczosWeightW(double d, int n, double scale)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / scale) * Sinc(d / n);
+            }
+        }
+        private double GetLanczosWeightX(double d, int n, double scale)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / scale) * Sinc(d / (n * scale));
+            }
+        }
+        private double GetLanczosWeightY(double d, int n, double scale)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / (scale - 1)) * Sinc(d / (n * scale));
+            }
+        }
+        private double GetLanczosWeightZ(double d, int n, double scale)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                return Sinc(d / (scale * 0.75)) * Sinc(d / (n * scale));
+            }
+        }
+        private double GetLanczosWeightAA(double d, int n, double scale)
+        {
+            if (d == 0) return 1.0;
+            else if (d > n) return 0.0;
+            else
+            {
+                //return Sinc(d / (scale * 0.75)) * Sinc(d / n);
+                return Sinc(d / (scale * 0.75)) * Sinc(d / n * (scale * 0.75));
             }
         }
 
@@ -683,6 +788,147 @@ namespace _20210429_Lanczosっぽいなにかできれいな縮小
                     xSum += x;
                     xw[i + n] = x;
                     double y = myFunc(Math.Abs(dy + i), n);
+                    ySum += y;
+                    yw[i + n] = y;
+                }
+
+                //重み合計で割り算して修正、全体で100%(1.0)にする
+                for (int i = 0; i < nn; i++)
+                {
+                    xw[i] /= xSum;
+                    yw[i] /= ySum;
+                }
+
+                // x * y
+                double[,] ws = new double[nn, nn];
+                for (int y = 0; y < nn; y++)
+                {
+                    for (int x = 0; x < nn; x++)
+                    {
+                        ws[x, y] = xw[x] * yw[y];
+                    }
+                }
+                return ws;
+            }
+        }
+
+
+        /// <summary>
+        /// 画像の拡大縮小、ランチョス法で補完、PixelFormats.Bgra32専用)
+        /// 
+        /// </summary>
+        /// <param name="source">PixelFormats.Bgra32のBitmap</param>
+        /// <param name="width">変換後の横ピクセル数を指定</param>
+        /// <param name="height">変換後の縦ピクセル数を指定</param>
+        /// <param name="n">最大参照距離、3か4がいい</param>
+        /// <returns></returns>
+        private BitmapSource LanczosBgra32(BitmapSource source, int width, int height, int n, double scale, Func<double, int, double, double> myFunc)
+        {
+            //1ピクセルあたりのバイト数、Byte / Pixel
+            int pByte = (source.Format.BitsPerPixel + 7) / 8;
+
+            //元画像の画素値の配列作成
+            int sourceWidth = source.PixelWidth;
+            int sourceHeight = source.PixelHeight;
+            int sourceStride = sourceWidth * pByte;//1行あたりのbyte数
+            byte[] sourcePixels = new byte[sourceHeight * sourceStride];
+            source.CopyPixels(sourcePixels, sourceStride, 0);
+
+            //変換後の画像の画素値の配列用
+            double widthScale = (double)sourceWidth / width;//横倍率
+            double heightScale = (double)sourceHeight / height;
+            int stride = width * pByte;
+            byte[] pixels = new byte[height * stride];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //参照点
+                    double rx = (x + 0.5) * widthScale;
+                    double ry = (y + 0.5) * heightScale;
+                    //参照点四捨五入で基準
+                    int xKijun = (int)(rx + 0.5);
+                    int yKijun = (int)(ry + 0.5);
+                    //修正した重み取得
+                    var ws = GetFixWeights(rx, ry, n);
+
+                    double bSum = 0, gSum = 0, rSum = 0, aSum = 0;
+                    double alphaFix = 0;
+                    //参照範囲は基準から上(xは左)へn、下(xは右)へn-1の範囲
+                    for (int yy = -n; yy < n; yy++)
+                    {
+                        int yc = yKijun + yy;
+                        //マイナス座標や画像サイズを超えていたら、収まるように修正
+                        yc = yc < 0 ? 0 : yc > sourceHeight - 1 ? sourceHeight - 1 : yc;
+                        for (int xx = -n; xx < n; xx++)
+                        {
+                            int xc = xKijun + xx;
+                            xc = xc < 0 ? 0 : xc > sourceWidth - 1 ? sourceWidth - 1 : xc;
+                            int pp = (yc * sourceStride) + (xc * pByte);
+                            double weight = ws[xx + n, yy + n];
+                            //完全透明ピクセル(a=0)だった場合はRGBは計算しないで
+                            //重みだけ足し算して後で使う
+                            if (sourcePixels[pp + 3] == 0)
+                            {
+                                alphaFix += weight;
+                                continue;
+                            }
+                            bSum += sourcePixels[pp] * weight;
+                            gSum += sourcePixels[pp + 1] * weight;
+                            rSum += sourcePixels[pp + 2] * weight;
+                            aSum += sourcePixels[pp + 3] * weight;
+                        }
+                    }
+
+                    //                    C#、WPF、バイリニア法での画像の拡大縮小変換、半透明画像(32bit画像)対応版 - 午後わてんのブログ
+                    //https://gogowaten.hatenablog.com/entry/2021/04/17/151803#32bit%E3%81%A824bit%E3%81%AF%E9%81%95%E3%81%A3%E3%81%9F
+                    //完全透明ピクセルによるRGB値の修正
+                    //参照範囲がすべて完全透明だった場合は0のままでいいので計算しない
+                    if (alphaFix == 1) continue;
+                    //完全透明ピクセルが混じっていた場合は、その分を差し引いてRGB修正する
+                    double rgbFix = 1 / (1 - alphaFix);
+                    bSum *= rgbFix;
+                    gSum *= rgbFix;
+                    rSum *= rgbFix;
+
+                    //0～255の範囲を超えることがあるので、修正
+                    bSum = bSum < 0 ? 0 : bSum > 255 ? 255 : bSum;
+                    gSum = gSum < 0 ? 0 : gSum > 255 ? 255 : gSum;
+                    rSum = rSum < 0 ? 0 : rSum > 255 ? 255 : rSum;
+                    aSum = aSum < 0 ? 0 : aSum > 255 ? 255 : aSum;
+
+                    int ap = (y * stride) + (x * pByte);
+                    pixels[ap] = (byte)(bSum + 0.5);
+                    pixels[ap + 1] = (byte)(gSum + 0.5);
+                    pixels[ap + 2] = (byte)(rSum + 0.5);
+                    pixels[ap + 3] = (byte)(aSum + 0.5);
+                }
+            };
+
+            BitmapSource bitmap = BitmapSource.Create(width, height, 96, 96, source.Format, null, pixels, stride);
+            return bitmap;
+
+            //修正した重み取得
+            double[,] GetFixWeights(double rx, double ry, int n)
+            {
+                int nn = n * 2;//全体の参照距離
+                //基準になる距離計算
+                double sx = rx - (int)rx;
+                double sy = ry - (int)ry;
+                double dx = (sx < 0.5) ? 0.5 - sx : 0.5 - sx + 1;
+                double dy = (sy < 0.5) ? 0.5 - sy : 0.5 - sy + 1;
+
+                //各ピクセルの重みと、重み合計を計算
+                double[] xw = new double[nn];
+                double[] yw = new double[nn];
+                double xSum = 0, ySum = 0;
+                for (int i = -n; i < n; i++)
+                {
+                    double x = myFunc(Math.Abs(dx + i), n, scale);
+                    xSum += x;
+                    xw[i + n] = x;
+                    double y = myFunc(Math.Abs(dy + i), n, scale);
                     ySum += y;
                     yw[i + n] = y;
                 }
@@ -1417,6 +1663,84 @@ namespace _20210429_Lanczosっぽいなにかできれいな縮小
             }
         }
 
-     
+        private void MyButton19_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightR, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value);
+        }
+
+        private void MyButton20_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightS, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value);
+        }
+
+        private void MyButton21_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightT, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value);
+        }
+
+        private void MyButton22_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightU, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value);
+        }
+
+        private void MyButton23_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightV, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value);
+        }
+
+        private void MyButton24_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightW, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value, MySliderScale.Value);
+        }
+
+        private void MyButton25_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightX, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value, MySliderScale.Value);
+        }
+
+        private void MyButton26_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightY, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value, MySliderScale.Value);
+        }
+
+        private void MyButton27_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightZ, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value, MySliderScale.Value);
+        }
+
+        private void MyButton28_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyBitmapOrigin == null) return;
+            int yoko = (int)Math.Ceiling(MyBitmapOrigin.PixelWidth / MySliderScale.Value);
+            int tate = (int)Math.Ceiling(MyBitmapOrigin.PixelHeight / MySliderScale.Value);
+            MyExe2(LanczosBgra32, GetLanczosWeightAA, MyBitmapOrigin32bit, yoko, tate, (int)MySlider.Value, MySliderScale.Value);
+        }
     }
 }
