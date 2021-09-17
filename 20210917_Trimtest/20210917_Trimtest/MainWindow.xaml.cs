53e7adb0fa93bac0b18f9b6ef27eae6e57eddfd4
waten <gogowaten@yahoo.co.jp>
2021-09-17 15:18:41 +0900

    202109171518

--- 20210917_Trimtest/20210917_Trimtest/MainWindow.xaml.cs
+++ 20210917_Trimtest/20210917_Trimtest/MainWindow.xaml.cs
@@ -44,7 +44,17 @@ namespace _20210917_Trimtest
             var cl = Environment.GetCommandLineArgs();
             this.Title = APP_NAME + " ver " + System.Diagnostics.FileVersionInfo.GetVersionInfo(cl[0]).FileVersion;
 
+            MyListBox.SelectionChanged += MyListBox_SelectionChanged;
+
+
             InitializeAppData();
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0) return;
+
+            MyImage.Source = MakeBitmapSourceBgra32FromFile((string)e.AddedItems[0]);
         }
 
         private void InitializeAppData()
@@ -129,6 +139,89 @@ namespace _20210917_Trimtest
         }
         #endregion アプリの設定ファイルの読み書き
 
+        #region 画像保存
+
+        private void SaveAllImage()
+        {
+            //MyProgressBar.Value = 0;
+            if (MyListBox.ItemsSource == null) return;
+
+            BitmapSource source;
+            Int32Rect rect = MakeTrimRect();
+            List<string> list = MyListBox.ItemsSource.Cast<string>().ToList();
+            double count = 0;
+
+            foreach (string sourcePath in list)
+            {
+                source = MakeBitmapSourceBgra32FromFile(sourcePath);
+
+                string folderPath = System.IO.Path.GetDirectoryName(sourcePath);
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(sourcePath);
+
+                fileName += "_trim.png";
+                string savePath = System.IO.Path.Combine(folderPath, fileName);
+
+                SaveTrimImage(source, savePath, rect);
+
+                count++;
+             
+                
+            }
+        }
+        private Int32Rect MakeTrimRect()
+        {
+            return new Int32Rect(MyAppData.TrimLeft, MyAppData.TrimTop, MyAppData.TrimWidth, MyAppData.TrimHeight);
+        }
+        private void SaveTrimImage(BitmapSource source, string path, Int32Rect rect)
+        {
+            BitmapSource trimImage = new CroppedBitmap(source, rect);
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(trimImage));
+            using (System.IO.FileStream fs = new(path, System.IO.FileMode.Create))
+            {
+                encoder.Save(fs);
+            }
+
+        }
+
+        #endregion 画像保存
+
+        #region 画像読み込み
+
+        /// <summary>
+        /// 画像ファイルパスからPixelFormats.Bgra32のBitmapSource作成
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="dpiX"></param>
+        /// <param name="dpiY"></param>
+        /// <returns></returns>
+        private BitmapSource MakeBitmapSourceBgra32FromFile(string filePath, double dpiX = 96, double dpiY = 96)
+        {
+            BitmapSource source = null;
+            try
+            {
+                using (var stream = System.IO.File.OpenRead(filePath))
+                {
+                    source = BitmapFrame.Create(stream);
+                    if (source.Format != PixelFormats.Bgra32)
+                    {
+                        source = new FormatConvertedBitmap(source, PixelFormats.Bgra32, null, 0);
+                    }
+                    int w = source.PixelWidth;
+                    int h = source.PixelHeight;
+                    int stride = (w * source.Format.BitsPerPixel + 7) / 8;
+                    byte[] pixels = new byte[h * stride];
+                    source.CopyPixels(pixels, stride, 0);
+                    source = BitmapSource.Create(w, h, dpiX, dpiY, source.Format, source.Palette, pixels, stride);
+                };
+            }
+            catch (Exception)
+            { }
+            return source;
+        }
+
+        #endregion 画像読み込み
+
 
         //アプリ終了時
         private void Window_Closed(object sender, EventArgs e)
@@ -140,9 +233,33 @@ namespace _20210917_Trimtest
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop) == false) return;
 
+            string[] datas = (string[])e.Data.GetData(DataFormats.FileDrop);
+            List<string> paths = datas.ToList();
+            paths.Sort();
+
+            MyListBox.ItemsSource = paths;
+            MyListBox.SelectedIndex = 0;
+        }
+
+        private void MyButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            MyProgressBar.IsIndeterminate = true;
+            SaveAllImage();
+            MyProgressBar.IsIndeterminate = false;
+        }
+
+        private void MyButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
+
+
+
+
+
 
 
     [DataContract]
@@ -162,6 +279,7 @@ namespace _20210917_Trimtest
 
         //jpeg品質
         [DataMember] public int JpegQuality { get; set; } = 90;
+
 
 
     }
