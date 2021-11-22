0d56e624e2791a9a71a4488ff2c75e63e50218fd
waten <gogowaten@yahoo.co.jp>
2021-11-22 20:55:28 +0900

    202111222055

---
+++ 20211122_textbox_template/20211122_textbox_template/MyTextBox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Globalization;
+
+
+namespace _20211122_textbox_template
+{
+    public class MyTextBox : TextBox
+    {
+        public MyTextBox()
+        {
+            Foreground = Brushes.Transparent;
+            Background = Brushes.Transparent;
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            FormattedText formatted = new(
+                Text,
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(
+                    FontFamily,
+                    FontStyle,
+                    FontWeight,
+                    FontStretch),
+                FontSize,
+                Brushes.Red,
+                96);
+
+            //縁取りが必要なければ
+            //drawingContext.DrawText(formatted, new Point());
+
+            //縁取りありはGeometry
+            Geometry geo = formatted.BuildGeometry(new Point());
+            drawingContext.DrawGeometry(Brushes.Green, new Pen(Brushes.Cyan, 2), geo);
+
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            InvalidateVisual();
+        }
+    }
+}
---
+++ 20211122_textbox_template/20211122_textbox_template/MyTextBoxTemplate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Globalization;
+
+namespace _20211122_textbox_template
+{
+    class MyTextBoxTemplate : TextBox
+    {
+        public Grid RootPanel;
+
+        public MyTextBoxTemplate()
+        {
+            Foreground = Brushes.Transparent;
+            Background = Brushes.Transparent;
+
+            //ControlTemplate template = new(typeof(TextBox));
+            //template.VisualTree = new FrameworkElementFactory(typeof(Grid), "root");
+            //Template = template;
+            //ApplyTemplate();            
+            //RootPanel = (Grid)template.FindName("root", this);
+            
+            //RootPanel.Background = Brushes.GreenYellow;
+
+            //ScrollViewer scrollViewer = new();
+            //scrollViewer.VerticalAlignment = VerticalAlignment.Center;
+
+            //ContentPresenter contentPresenter = new();
+            //contentPresenter.Content = new TextBox();
+            //RootPanel.Children.Add(scrollViewer);
+            //RootPanel.Children.Add(contentPresenter);
+
+            
+
+            this.BorderThickness = new Thickness(0);
+            //this.BorderBrush = null;
+            
+
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            FormattedText formatted = new(
+                Text,
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(
+                    FontFamily,
+                    FontStyle,
+                    FontWeight,
+                    FontStretch),
+                FontSize,
+                Brushes.Red,
+                96);
+
+            //縁取りが必要なければ
+            //drawingContext.DrawText(formatted, new Point());
+
+            //縁取りありはGeometry
+            Geometry geo = formatted.BuildGeometry(new Point());
+            drawingContext.DrawGeometry(Brushes.Green, new Pen(Brushes.Cyan, 2), geo);
+
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            InvalidateVisual();
+        }
+
+
+
+    }
+}
