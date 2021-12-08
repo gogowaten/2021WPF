a9667d3b9130fe8a57a7416cbf66960e8e39c2f9
waten <gogowaten@yahoo.co.jp>
2021-12-08 21:11:35 +0900

    202112082111

--- 20211206_FontList作成/20211206_FontList作成/MainWindow.xaml.cs
+++ 20211206_FontList作成/20211206_FontList作成/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Globalization;
+using System.IO;
 
 //SystemFontFamiliesを表示する part1 : Win32 & wpf メモ
 //http://blog.livedoor.jp/oans/archives/54753149.html
@@ -25,44 +26,249 @@ namespace _20211206_FontList作成
     public partial class MainWindow : Window
     {
         List<FontData> FontDatas = new();
+
         public MainWindow()
         {
             InitializeComponent();
-
-         
-
-            Uri uri = new("file:///C:/WINDOWS/FONTS/MEIRYO.TTC#2");
-            //Uri uri = new(@"C:/WINDOWS/FONTS/MEIRYO.TTC#2");//これは後で使うときにエラーになる
-            //Uri uri = new(@"file:///C:/WINDOWS/FONTS/MEIRYO.TTC#2");
-            //Uri uri = new(@"C:\Windows\Fonts\meiryo.ttc");
-            Glyphs glyphs = new();
-            glyphs.FontUri = uri;
-            GlyphTypeface glyphTypeface = new(uri);
-            var fns = glyphTypeface.FamilyNames;
-            var facens = glyphTypeface.FaceNames;
-            MyTextBlock.FontFamily = new(uri, "Meiryo Ui");
-
-            //以下は同じ効果
-            //this.FontFamily = new(new Uri(@"C:/Windows/Fonts/Meiryo.ttc"), "Meiryo UI");            
-            //this.FontFamily = new("Meiryo UI");
-
-            ////インストールされていないフォントは使えないみたい
-            ////以下はエラーにはならないけど反映されず初期値のFontFamilyが使われる
-            //uri = new(@"I:\4D080K4\hdd0\Program Files\Pinnacle\Pinnacle Express\Fonts\Pretext_.ttf");
-            //MyTextBlock.FontFamily = new(uri, "PRETEXT");
-
-
 
             //今のPCで使っている言語(日本語)取得
             var language =
                 System.Windows.Markup.XmlLanguage.GetLanguage(
                 CultureInfo.CurrentCulture.IetfLanguageTag);
             var culture = CultureInfo.CurrentCulture;
+            CultureInfo cultureUS = new("en-US");
+
+
+
+
+
+            List<Uri> uris = new();
+            List<string> absUri = new();
+            List<string> absPath = new();
+            List<string> origin = new();
+            List<string> fragment = new();
+
+            List<string> uOrigin = new();//重複なしのUri
+            List<string> uName = new();//重複なしのフォント名
+            List<string> uName2 = new();//重複なしのフォント名
+            List<FontFamily> fontFamilies1 = new();
+            Dictionary<string, FontFamily> fontDict = new();
+
+            //SystemFontFamiliesだけでは全てのフォントが取得できないので
+            //GlyphTypefaceのWin32FamilyNames
+            foreach (var item in Fonts.SystemFontFamilies)
+            {
+                var typefaces = item.GetTypefaces();
+                foreach (var typeface in typefaces)
+                {
+                    typeface.TryGetGlyphTypeface(out GlyphTypeface gType);
+                    if (gType != null)
+                    {
+                        //フォント名取得はFamilyNamesではなく、Win32FamilyNamesを使う
+                        //FamilyNamesだと違うフォントなのに同じフォント名で取得されるものがあるので
+                        //Win32FamilyNamesを使う
+                        //日本語名がなければ英語名
+                        string fontName = gType.Win32FamilyNames[culture] ?? gType.Win32FamilyNames[cultureUS];
+                        //string fontName = gType.FamilyNames[culture] ?? gType.FamilyNames[cultureUS];
+
+                        //フォント名で重複判定
+                        var uri = gType.FontUri;                        
+                        if (uName.Contains(fontName) == false)
+                        {
+                            uName.Add(fontName);
+                            FontData fontData1 = new();
+                            fontData1.FontFamily = new(uri, fontName);
+                            fontData1.FamilyName = fontName;
+                            fontData1.Uri = uri;
+                            FontDatas.Add(fontData1);
+                            fontDict.Add(fontName, new(uri, fontName));
+                        }
+                    }
+                }
+            }
+            SortedDictionary<string, FontFamily> sorted = new(fontDict);
+
+            var neko2 = FontDatas;//376種類
+
+            ////フォントファイル一覧からも追加していく、けど全て含まれていた
+            //var allUris = GetFontUris();
+            //List<FontFamily> ffl = new();
+            //List<Uri> errors = new();
+            ////フォントファイルからフォント名を取得するためにGlyphTypefaceを作成
+            ////フォントによってはGlyphTypefaceを作成できないものがあったのでtryを使っている
+            //foreach (var uri in allUris)
+            //{
+            //    try
+            //    {
+            //        GlyphTypeface typeface = new(uri);                    
+            //        var fintName = typeface.FamilyNames[culture] ?? typeface.FamilyNames[cultureUS];
+
+            //        ffl.Add(new FontFamily(uri, fintName));//414
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        errors.Add(uri);
+            //    }
+            //}
+
+            //foreach (var item in ffl)
+            //{
+            //    var typefaces = item.GetTypefaces();
+            //    foreach (var typeface in typefaces)
+            //    {
+            //        typeface.TryGetGlyphTypeface(out GlyphTypeface gType);
+            //        if (gType != null)
+            //        {
+            //            var uri = gType.FontUri;
+            //            string fontName = gType.Win32FamilyNames[culture] ?? gType.Win32FamilyNames[cultureUS];
+            //            origin.Add(uri.OriginalString);
+            //            if (uName.Contains(fontName) == false)
+            //            {
+            //                uName.Add(fontName);
+            //                FontData fontData1 = new();
+            //                fontData1.FontFamily = new(uri, fontName);
+            //                fontData1.FamilyName = fontName;
+            //                fontData1.Uri = uri;
+            //                FontDatas.Add(fontData1);
+            //            }
+            //        }
+            //    }
+            //}
+
+
+            neko2 = FontDatas;
+
+            //foreach (var item in Fonts.SystemFontFamilies)
+            //{
+            //    var typefaces = item.GetTypefaces();
+            //    foreach (var typeface in typefaces)
+            //    {
+            //        typeface.TryGetGlyphTypeface(out GlyphTypeface gType);
+            //        if (gType != null)
+            //        {
+            //            //AbsoluteUriとOriginalStringは同じで末尾に#1とかがついている
+            //            var uri = gType.FontUri;
+            //            absUri.Add(uri.AbsoluteUri);//パスに#1とかが付く
+            //            absPath.Add(uri.AbsolutePath);//パス
+            //            origin.Add(uri.OriginalString);//パスに#1とかが付く
+            //            fragment.Add(uri.Fragment);//#1とか#2など
+            //            uris.Add(gType.FontUri);
+
+            //            //重複なしで追加していく
+            //            if (uOrigin.Contains(uri.OriginalString) == false)
+            //            {
+            //                uOrigin.Add(uri.OriginalString);//パス(fragment付き)
+
+            //                //test
+            //                var ext = System.IO.Path.GetExtension(uri.LocalPath);
+
+            //                if(ext == ".ttc")
+            //                {
+
+            //                }
+            //                else
+            //                {
+
+            //                }
+
+            //                string name = gType.FamilyNames[culture];//日本語名
+            //                if (name == null) { name = item.Source; }//なければ英語名
+            //                string faceN;
+            //                FontData fontData = new();
+            //                //フェイス名、ボールドとかイタリックとか
+            //                if (uName.Contains(name))
+            //                {
+            //                    faceN = gType.FaceNames[culture];
+            //                    if (faceN == null)
+            //                    {
+            //                        faceN = gType.FaceNames[cultureUS];
+            //                    }
+            //                    name += " " + faceN;
+            //                    uName.Add(name);
+            //                    fontData.FamilyName = name;
+            //                }
+            //                else
+            //                {
+            //                    uName.Add(name);
+            //                    fontData.FamilyName = name;
+            //                }
+
+            //                FontFamily fontFamily = new(uri, name);
+            //                fontFamilies1.Add(fontFamily);
+            //                fontData.FontFamily = fontFamily;
+            //                fontData.Uri = uri;
+            //                FontDatas.Add(fontData);
+
+            //                continue;
+            //            }
+            //        }
+            //    }
+
+            //}
+
+
+
+
+            //MyListBox.ItemsSource = FontDatas;
+            MyListBox.ItemsSource = sorted;
+
+            List<string> fontPath = origin.Distinct().ToList();
+            List<Uri> uris2 = new();
+            List<FontFamily> fontFamilies = new();
+            List<Glyphs> gList = new();
+            List<GlyphTypeface> gTypes = new();
+            foreach (var item in fontPath)
+            {
+                Uri u = new(item);
+                uris2.Add(new(item));
+                fontFamilies.Add(new(u, ""));
+                Glyphs glyphs = new();
+                glyphs.FontUri = u;
+                gList.Add(glyphs);
+                GlyphTypeface glyphTypeface = new(u);
+                gTypes.Add(glyphTypeface);
+            }
+
+            var neko = uris;
+
+
+
+
+            //Uri uri = new("file:///C:/WINDOWS/FONTS/MEIRYO.TTC#2");
+            //Uri uri = new(@"C:/WINDOWS/FONTS/MEIRYO.TTC#2");//これは後で使うときにエラーになる
+            //Uri uri = new(@"file:///C:/WINDOWS/FONTS/MEIRYO.TTC#2");
+            //Uri uri = new(@"C:\Windows\Fonts\meiryo.ttc");
+            //Glyphs glyphs = new();
+            //glyphs.FontUri = uri;
+            //GlyphTypeface glyphTypeface = new(uri);
+            //var fns = glyphTypeface.FamilyNames;
+            //var facens = glyphTypeface.FaceNames;
+            //MyTextBlock.FontFamily = new(uri, "Meiryo Ui");
+
+            //以下は同じ効果
+            //this.FontFamily = new(new Uri(@"C:/Windows/Fonts/Meiryo.ttc"), "Meiryo UI");            
+            //this.FontFamily = new("Meiryo UI");
+
+            //
+            ////インストールされていないフォントは使えないみたい
+            ////以下はエラーにはならないけど反映されず初期値のFontFamilyが使われる
+            //Uri uri = new(@"I:\4D080K4\hdd0\Program Files\Pinnacle\Pinnacle Express\Fonts\Pretext_.ttf");
+            //FontFamily fontFamily1 = new(uri, "PRETEXT");
+            //MyTextBlock.FontFamily = fontFamily1;
+            //
+
+
+
+            ////今のPCで使っている言語(日本語)取得
+            //var language =
+            //    System.Windows.Markup.XmlLanguage.GetLanguage(
+            //    CultureInfo.CurrentCulture.IetfLanguageTag);
+            //var culture = CultureInfo.CurrentCulture;
 
             ////Fonts.SystemFontFamiliesの
             //foreach (var item in Fonts.SystemFontFamilies)
             //{
-            //    var neko= item.FamilyTypefaces;
+            //    var neko = item.FamilyTypefaces;
             //    var inu = item.GetTypefaces();
 
             //    ICollection<Typeface> types = item.GetTypefaces();
@@ -251,6 +457,41 @@ namespace _20211206_FontList作成
             //glyphs.UnicodeString = "abc";
             //MyGrid.Children.Add(glyphs);
         }
+
+        /// <summary>
+        /// インストールされている全てのフォントファイルのUriリスト作成
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<Uri> GetFontUris()
+        {
+            //systemのフォントフォルダから一覧
+            IEnumerable<Uri> result = MakeList(
+                Environment.GetFolderPath(Environment.SpecialFolder.Fonts));
+
+            //ユーザーのフォントフォルダが存在する場合は、
+            //それも取得してConcatで付け加える
+            string userPath = System.IO.Path.Combine(
+                Environment.GetFolderPath(
+                    Environment.SpecialFolder.LocalApplicationData),
+                @"Microsoft\Windows\Fonts");
+            if (System.IO.Directory.Exists(userPath))
+            {
+                result = result.Concat(MakeList(userPath));
+            }
+
+            //指定フォルダのフォントファイルすべてをUriにして取得
+            static IEnumerable<Uri> MakeList(string folderPath)
+            {
+                IEnumerable<Uri> result =
+                    System.IO.Directory.GetFiles(folderPath, "*.ttf").Concat
+                    (System.IO.Directory.GetFiles(folderPath, "*.ttc")).Concat
+                    (System.IO.Directory.GetFiles(folderPath, "*.otf")).Select
+                    (x => new Uri(x));
+                return result;
+            }
+            return result;
+        }
+
     }
 
     public class FontData
@@ -263,7 +504,7 @@ namespace _20211206_FontList作成
         public StyleSimulations StyleSimulations { get; set; }
         public FontWeight FontWeight { get; set; }
         public FontStyle FontStyle { get; set; }
-
+        public FontFamily FontFamily { get; set; }
         public override string ToString()
         {
             string str = FamilyName + "_" + FontWeight + "_" + FontStyle;
