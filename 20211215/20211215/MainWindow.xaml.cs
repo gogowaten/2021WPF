43e0c0dbba9d97f03f5b2021cecf31443f221d13
waten <gogowaten@yahoo.co.jp>
2021-12-16 22:23:57 +0900

    202112162223

--- 20211215/20211215/MainWindow.xaml.cs
+++ 20211215/20211215/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,100 @@ namespace _20211215
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ThumbEx MyThunbEx;
+        private ThumbEx MyThunbEx2;
+        private MainData MainData = new();
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = MainData;
+
+            Test1();
+            Test2();
+
+            MyThunbEx.DragDelta += MyThunbEx_DragDelta;
+            MyThunbEx2.DragDelta += MyThunbEx_DragDelta;
+            //DataContext = MyThunbEx.ThumbData;
+        }
+
+        private void MyThunbEx_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            var t = sender as ThumbEx;
+            Canvas.SetLeft(t, Canvas.GetLeft(t) + e.HorizontalChange);
+            Canvas.SetTop(t, Canvas.GetTop(t) + e.VerticalChange);
+        }
+
+
+        private void Test1()
+        {
+            TextBlock textBlock = new();
+            textBlock.Text = "TextBlock";
+            textBlock.Foreground = Brushes.Gray;
+            textBlock.Background = Brushes.Cyan;
+            textBlock.FontSize = 50;
+
+            MyThunbEx = new(textBlock);
+
+            MyCanvas.Children.Add(MyThunbEx);
+        }
+
+        private void Test2()
+        {
+            TextBlock textBlock = new();
+            textBlock.Text = "TextBlock1";
+            ThumbEx t1 = new(textBlock);
+
+            TextBlock textBlock2 = new();
+            textBlock2.Text = "TextBlock2";
+            ThumbEx t2 = new(textBlock2);
+            Canvas.SetLeft(t2, 100);
+            Canvas.SetTop(t2, 100);
+
+            TextBox textBox = new();
+            textBox.Text = "TextBox";
+            ThumbEx t3 = new(textBox);
+            Canvas.SetLeft(t3, 100);
+
+            MyThunbEx2 = new(t1);
+            MyThunbEx2.AddContent(t2);
+            MyThunbEx2.AddContent(t3);
+
+            MyCanvas.Children.Add(MyThunbEx2);
+        }
+
+        private void MyButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            //DataContext = MyThunbEx2;
+
+            //UIElementCollection neko = MyThunbEx2.RootCanvas.Children;
+            //Type type = neko[0].GetType();
+            var data = MainData.Left;
+            MainData.Left = 200;
+            var left = MyThunbEx.Left;
+            //MyThunbEx.Left = 100;
+            //left = MyThunbEx.Left;
+        }
+    }
+
+    public class MainData : System.ComponentModel.INotifyPropertyChanged
+    {
+        private double left;
+
+        public double Left
+        {
+            get => left; set
+            {
+                if (left == value) { return; }
+                left = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ 20211215/20211215/ThumbEx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+
+
+//DependencyProperty INotifyPropertyChanged
+
+//DPはBindingのソースとターゲットどちらでにもできる
+//INPCはソースには使えるけど、ターゲットには使えない
+namespace _20211215
+{
+    public class ThumbEx : Thumb
+    {
+        public Canvas RootCanvas;
+        private static readonly string RootName = "canvas";
+        public ThumbData ThumbData;
+
+        public ThumbEx(UIElement element)
+        {
+            ThumbData = new();
+            DataContext = ThumbData;
+
+            ControlTemplate template = new(typeof(Thumb));
+            template.VisualTree = new FrameworkElementFactory(typeof(Canvas), RootName);
+            Template = template;
+            _ = ApplyTemplate();
+            RootCanvas = (Canvas)Template.FindName(RootName, this);
+
+            Canvas.SetLeft(this, 0);
+            Canvas.SetTop(this, 0);
+
+            _ = RootCanvas.Children.Add(element);
+
+            Binding binding = new();
+            binding.Source = this;
+            binding.Path = new PropertyPath(Canvas.LeftProperty);
+            binding.Mode = BindingMode.TwoWay;
+            this.SetBinding(LeftProperty, binding);
+
+            //↑↓は同じ、BindingModeを双方向だから？
+
+            //Binding binding = new();
+            //binding.Source = this;
+            //binding.Path = new PropertyPath(LeftProperty);
+            //binding.Mode = BindingMode.TwoWay;
+            //this.SetBinding(Canvas.LeftProperty, binding);
+
+           
+            //binding = new();
+            //binding.Source = this;
+            //binding.Path = new PropertyPath(LeftProperty);
+            //binding.Mode = BindingMode.TwoWay;
+            //SetBinding(this.ThumbData.Left, binding);
+        }
+
+        public void AddContent(UIElement element)
+        {
+            RootCanvas.Children.Add(element);
+        }
+
+        //
+        #region DependencyProperty
+        //public double MyLeft { get; set; }
+
+        public double Left
+        {
+            get { return (double)GetValue(LeftProperty); }
+            set { SetValue(LeftProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Left.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LeftProperty =
+            DependencyProperty.Register("Left", typeof(double), typeof(ThumbEx), new PropertyMetadata(0.0));
+
+
+        public double Top
+        {
+            get { return (double)GetValue(TopProperty); }
+            set { SetValue(TopProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Top.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TopProperty =
+            DependencyProperty.Register("Top", typeof(double), typeof(ThumbEx), new PropertyMetadata(0.0));
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ThumbEx), new PropertyMetadata(""));
+
+
+
+        public List<ThumbData> ThumbDatas
+        {
+            get { return (List<ThumbData>)GetValue(ThumbDatasProperty); }
+            set { SetValue(ThumbDatasProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThumbDatas.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThumbDatasProperty =
+            DependencyProperty.Register("ThumbDatas", typeof(List<ThumbData>), typeof(ThumbEx), new PropertyMetadata(null));
+
+
+        #endregion DependencyProperty
+
+    }
+
+    public class ThumbData
+    {
+        public List<ThumbData> ThumbDatas { get; set; } = new();
+        public double Left { get; set; } = 0;
+        public double Top { get; set; } = 0;
+        public string Text { get; set; } = "";
+
+
+
+
+    }
+
+    public class ThumbDataNotify : System.ComponentModel.INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public List<ThumbData> ThumbDatas { get; set; } = new();
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public string Text { get; set; } = "";
+
+
+    }
+
+
+
+    public class ThumbDataDP : DependencyObject
+    {
+        public List<ThumbData> ThumbDatas { get; set; } = new();
+        public double Left { get; set; } = 0;
+        public double Top { get; set; } = 0;
+        public string Text { get; set; } = "";
+
+
+
+        public double MyLeft
+        {
+            get { return (double)GetValue(MyLeftProperty); }
+            set { SetValue(MyLeftProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyLeft.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyLeftProperty =
+            DependencyProperty.Register("MyLeft", typeof(double), typeof(ThumbEx), new PropertyMetadata(0.0));
+
+
+    }
+
+
+}
