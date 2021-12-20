6af59633737ebf4dfd11beb77f531f84081f62ef
waten <gogowaten@yahoo.co.jp>
2021-12-20 18:00:50 +0900

    202112201800

--- 20211220/20211220/MainWindow.xaml.cs
+++ 20211220/20211220/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace _20211220
 {
@@ -20,9 +23,108 @@ namespace _20211220
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
+        private void AddThumb()
+        {
+            ExThumb thumb = new(10, 10);
+            thumb.Width = 100;
+            thumb.Height = 60;
+
+            thumb.DragDelta += Thumb_DragDelta;
+
+            MyCanvas.Children.Add(thumb);
+
+            DataContext = thumb;
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            ExThumb thumb = sender as ExThumb;
+            thumb.Left += e.HorizontalChange;
+            thumb.Top += e.VerticalChange;
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddThumb();
+        }
+
+        private void ButtonCheck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    //public class Data : ObservableCollection<Data>, System.ComponentModel.INotifyPropertyChanged
+    //{
+    //    private double left;
+
+    //    //public ObservableCollection<Data> Children { get; set; } = new();
+    //    public double Left
+    //    {
+    //        get => left; set
+    //        {
+    //            left = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberNameAttribute] string name = null)
+    //    {
+    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //    }
+    //}
+
+    public class ExThumb : Thumb, System.ComponentModel.INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private double left;
+        public double Left
+        {
+            get => left; set
+            { left = value; OnPropertyChanged(); }
+        }
+
+        private double _Top;
+        public double Top
+        {
+            get => _Top;
+            set { _Top = value; OnPropertyChanged(); }
+        }
+
+
+        public ExThumb(double left, double top)
+        {
+            Left = left;
+            Top = top;
+
+            Binding b = new();
+            b.Source = this;
+            b.Path = new PropertyPath("Left");
+            b.Mode = BindingMode.TwoWay;//必須
+            SetBinding(Canvas.LeftProperty, b);
+
+            //SetBinding(Canvas.LeftProperty, new Binding("Left"));//TwoWayがないから動かない
+            //this.SetBinding(Canvas.LeftProperty, "Left");//TwoWayがないから動かない
+
+            b = new();
+            b.Source = this;
+            b.Path = new PropertyPath("Top");
+            b.Mode = BindingMode.TwoWay;
+            SetBinding(Canvas.TopProperty, b);
+
+        }
+
     }
 }
---
+++ 2021122014/2021122014/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+//ItemsControl 攻略 ～ 外観のカスタマイズ | grabacr.nét
+//http://grabacr.net/archives/1240
+
+//ListBoxのTemplate変更をC#で
+namespace _2021122014
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+            ListBox listBox = new();
+            MyGrid.Children.Add(listBox);
+
+            ////FrameworkElementFactory rootCanvas = new(typeof(Canvas), "root");
+            //ItemsPanelTemplate panelTemplate = new(new FrameworkElementFactory(typeof(Canvas)));
+            //listBox.ItemsPanel = panelTemplate;
+
+            ////1. Template = ControlTemplate はアイテム表示欄全体の設定になる
+            //FrameworkElementFactory factory = new(typeof(Border));
+            //factory.SetValue(Border.BorderBrushProperty, Brushes.Magenta);
+            //factory.SetValue(Border.BorderThicknessProperty, new Thickness(1));
+            //factory.SetValue(Border.BackgroundProperty, Brushes.LightGray);
+            //factory.SetValue(Border.MarginProperty, new Thickness(10));
+            //factory.AppendChild(new FrameworkElementFactory(typeof(ItemsPresenter)));//これがよくわからん、アイテム表示になるみたい
+            //ControlTemplate controlTemplate = new(typeof(ItemsControl));
+            //controlTemplate.VisualTree = factory;
+            //ItemsControl itemsControl = new();
+            //itemsControl.Template = controlTemplate;
+            //listBox.Template = controlTemplate;
+
+            ////2. ItemsPanel = ItemsPanelTemplate、アイテム表示欄のベースになるパネルの設定
+            //FrameworkElementFactory root = new(typeof(VirtualizingStackPanel));
+            //root.SetValue(VirtualizingStackPanel.OrientationProperty, Orientation.Horizontal);
+            //ItemsPanelTemplate template = new(root);
+            //listBox.ItemsPanel = template;
+
+            //3. 1と2をあわせて
+            FrameworkElementFactory factory = new(typeof(Border));
+            factory.SetValue(Border.BorderBrushProperty, Brushes.Magenta);
+            factory.SetValue(Border.BorderThicknessProperty, new Thickness(1));
+            factory.SetValue(Border.BackgroundProperty, Brushes.LightGray);
+            factory.SetValue(Border.MarginProperty, new Thickness(10));
+            //factory.AppendChild(new FrameworkElementFactory(typeof(ItemsPresenter)));//これがよくわからん、アイテム表示になるみたい
+            FrameworkElementFactory root = new(typeof(VirtualizingStackPanel));
+            root.SetValue(VirtualizingStackPanel.OrientationProperty, Orientation.Horizontal);            
+            root.SetValue(VirtualizingStackPanel.IsItemsHostProperty, true);
+            factory.AppendChild(root);//ここが違う
+            ControlTemplate controlTemplate = new(typeof(ItemsControl));
+            controlTemplate.VisualTree = factory;
+            ItemsControl itemsControl = new();
+            itemsControl.Template = controlTemplate;
+            listBox.Template = controlTemplate;
+
+
+            listBox.ItemsSource = new List<int>() { 1, 2, 3 };
+
+        }
+    }
+
+    public class ExListBox : ListBox
+    {
+        public ExListBox()
+        {
+            DataTemplate template = new(typeof(ListBox));
+            template.VisualTree = new FrameworkElementFactory(typeof(Canvas), "root");
+            this.ItemTemplate = template;
+            ApplyTemplate();
+            Canvas canvas = (Canvas)template.FindName("root", this);
+
+            Thumb t = new();
+            t.Width = 100;
+            t.Height = 20;
+            canvas.Children.Add(t);
+            ItemsControl itemsControl = new();
+            itemsControl.ItemTemplate = template;
+
+        }
+    }
+}
