b052f721e7a37a45b237577dec44c7f4b9448465
waten <gogowaten@yahoo.co.jp>
2021-12-22 15:56:34 +0900

    202112221556

---
+++ 20211222/20211222/ExThumb.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace _20211222
+{
+
+    public class ExThumb : Thumb, INotifyPropertyChanged
+    {
+        private readonly string ROOT_PANEL_NAME = "rootPanel";
+        private Canvas RootCanvas;
+        private MainWindow MyMainWindow;
+        public bool IsGroup;
+        public ExThumb RootExThumb;
+        private double left;
+        private double top;
+
+        public ObservableCollection<ExThumb> Children { get; set; } = new();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public double Left { get => left; set { left = value; OnPropertyChanged(); } }
+        public double Top { get => top; set { top = value; OnPropertyChanged(); } }
+
+
+        public ExThumb()
+        {
+
+        }
+        public ExThumb(MainWindow mainWindow, string name, double left = 0, double top = 0)
+        {
+            MyMainWindow = mainWindow;
+
+            ControlTemplate template = new(typeof(Thumb));
+            template.VisualTree = new FrameworkElementFactory(typeof(Canvas), ROOT_PANEL_NAME);
+            Template = template;
+            ApplyTemplate();
+            RootCanvas = Template.FindName(ROOT_PANEL_NAME, this) as Canvas;
+            RootCanvas.Background = Brushes.Transparent;
+            Focusable = true;
+
+            RootExThumb = this;
+
+            Left = left;
+            Top = top;
+            Name = name;
+            DragDelta += ExThumb_DragDelta;
+            GotFocus += ExThumb_GotFocus;
+            SetBinding(Canvas.LeftProperty, MakeBinding("Left"));
+            SetBinding(Canvas.TopProperty, MakeBinding("Top"));
+
+
+        }
+
+        private void ExThumb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            MyMainWindow.DataContext = this.RootExThumb;
+            MyMainWindow.CurrentExThumb = this.RootExThumb;
+        }
+
+        public ExThumb(MainWindow mainWindow, string name, UIElement element, double left = 0, double top = 0) : this(mainWindow, name, left, top)
+        {
+            AddChildrenElement(element);
+        }
+
+        private void ExThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            ExThumb ex = sender as ExThumb;
+            if (ex.Name == ex.RootExThumb.Name)
+            {
+                Left += e.HorizontalChange;
+                Top += e.VerticalChange;
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+        private Binding MakeBinding(string pName)
+        {
+            Binding b = new();
+            b.Source = this;
+            b.Path = new PropertyPath(pName);
+            b.Mode = BindingMode.TwoWay;
+            return b;
+        }
+        public void AddChildrenExThumb(ExThumb exThumb)
+        {
+            RootCanvas.Children.Add(exThumb);
+            Children.Add(exThumb);
+            IsGroup = true;
+            exThumb.RootExThumb = this;
+            exThumb.DragDelta -= ExThumb_DragDelta;
+        }
+        public void AddChildrenElement(UIElement element)
+        {
+            RootCanvas.Children.Add(element);
+
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"name({Name}), (x,y) = ({Left},{Top})";
+        }
+    }
+}
---
+++ 20211222/20211222/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20211222
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ExThumb CurrentExThumb = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+       
+            Test1();
+            GroupTest1();
+
+        }
+
+       
+
+        private void Test1()
+        {
+            TextBlock textBlock1 = new();
+            textBlock1.Text = "textBlock1";
+            textBlock1.FontSize = 30;
+            textBlock1.Background = Brushes.Cyan;
+            ExThumb exThumb1 = new(this, "test1", textBlock1, 10, 10);
+            MyCanvas.Children.Add(exThumb1);
+
+        }
+        private void GroupTest1()
+        {
+            TextBlock t1 = MakeTextBloxk("textBlock1", 30, Brushes.Magenta);
+            ExThumb ex1 = new(this, "text1", t1, 0, 0);
+            TextBlock t2 = MakeTextBloxk("textBlock2", 30, Brushes.MediumAquamarine);
+            ExThumb ex2 = new(this, "text2", t2, 100, 100);
+            ExThumb exGroup = new(this, "Group", 100, 100);
+            exGroup.AddChildrenExThumb(ex1);
+            exGroup.AddChildrenExThumb(ex2);
+            MyCanvas.Children.Add(exGroup);
+            
+        }
+        private TextBlock MakeTextBloxk(string text, double fontsize, Brush brush)
+        {
+            TextBlock textBlock = new();
+            textBlock.Text = text;
+            textBlock.FontSize = fontsize;
+            textBlock.Background = brush;
+            return textBlock;
+        }
+
+        private void ButtonTest1_Click(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext;
+            var cex = CurrentExThumb;
+        }
+    }
+}
