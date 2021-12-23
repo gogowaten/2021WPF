e85d5e3b100db56c1f8d675a676fcf0704eec0f1
waten <gogowaten@yahoo.co.jp>
2021-12-23 19:57:03 +0900

    202112231957

---
+++ 20211223/20211223/ExThumb.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace _20211223
+{
+    public class ExThumb : Thumb, INotifyPropertyChanged
+    {
+        private readonly string ROOT_PANEL_NAME = "rootPanel";
+        private Canvas RootCanvas;
+        private MainWindow MyMainWindow;
+        public bool IsGroup;
+        //public bool IsRootExThumb;
+        public bool IsLayer;
+        public ExThumb ParentExThumb;
+
+        public ExThumb RootExThumb;
+        private double left;
+        private double top;
+
+        public ObservableCollection<ExThumb> Children { get; set; } = new();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public double Left { get => left; set { left = value; OnPropertyChanged(); } }
+        public double Top { get => top; set { top = value; OnPropertyChanged(); } }
+
+
+
+        public ExThumb(MainWindow mainWindow, string name, double left = 0, double top = 0)
+        {
+            MyMainWindow = mainWindow;
+
+            ControlTemplate template = new(typeof(Thumb));
+            template.VisualTree = new FrameworkElementFactory(typeof(Canvas), ROOT_PANEL_NAME);
+            Template = template;
+            ApplyTemplate();
+            RootCanvas = Template.FindName(ROOT_PANEL_NAME, this) as Canvas;
+            RootCanvas.Background = Brushes.Transparent;
+            Focusable = true;
+
+            RootExThumb = this;
+
+            Left = left;
+            Top = top;
+            Name = name;
+            DragDelta += ExThumb_DragDelta;
+            GotFocus += ExThumb_GotFocus;
+            SetBinding(Canvas.LeftProperty, MakeBinding("Left"));
+            SetBinding(Canvas.TopProperty, MakeBinding("Top"));
+
+
+        }
+
+        private void ExThumb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            MyMainWindow.CurrentExThumb = this.RootExThumb;
+            //オリジナルソース以外には通知しないで、ここで切断
+            e.Handled = true;
+        }
+
+        public ExThumb(MainWindow mainWindow, string name, UIElement element, double left = 0, double top = 0) : this(mainWindow, name, left, top)
+        {
+            AddChildrenElement(element);
+        }
+
+        private void ExThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Left += e.HorizontalChange;
+            Top += e.VerticalChange;
+        }
+
+
+        private Binding MakeBinding(string pName)
+        {
+            Binding b = new();
+            b.Source = this;
+            b.Path = new PropertyPath(pName);
+            b.Mode = BindingMode.TwoWay;
+            return b;
+        }
+
+
+        //グループ化のときに使う
+        public void AddChildrenExThumb(ExThumb exThumb)
+        {
+            RootCanvas.Children.Add(exThumb);
+            Children.Add(exThumb);
+            IsGroup = true;
+            exThumb.ParentExThumb = this;
+
+            //ルートを取得、設定
+            ExThumb rootEx = GetRootExThumb(exThumb);
+            exThumb.RootExThumb = rootEx;
+            //追加Thumbがルートじゃないときは動かないようにDragdeltaを外す
+            if (exThumb != rootEx)
+            {
+                exThumb.DragDelta -= exThumb.ExThumb_DragDelta;
+            }
+
+
+
+        }
+        //ルートThumbを取得
+        private ExThumb GetRootExThumb(ExThumb ex)
+        {
+            //Parentを辿る、Parentが無くなるかParentがLayer用Thumbになるまで辿る
+            if (ex.ParentExThumb == null || ex.ParentExThumb.IsLayer) { return ex; }
+            while (true)
+            {
+                GetRootExThumb(ex.ParentExThumb);
+            }
+        }
+
+        public void RemoveChildrenExThumb(ExThumb exThumb)
+        {
+            exThumb.ParentExThumb.RootCanvas.Children.Remove(exThumb);
+            exThumb.RootExThumb = exThumb;
+            exThumb.DragDelta += exThumb.ExThumb_DragDelta;
+        }
+
+        public void AddChildrenElement(UIElement element)
+        {
+            RootCanvas.Children.Add(element);
+
+        }
+        public void AddDragDeltaEvent()
+        {
+            DragDelta += ExThumb_DragDelta;
+        }
+        public void RemoveDragDeltaEvent()
+        {
+            DragDelta -= ExThumb_DragDelta;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"name({Name}), (x,y) = ({Left},{Top})";
+        }
+    }
+}
---
+++ 20211223/20211223/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace _20211223
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ExThumb currentExThumb;
+        //private ExThumb MyExThumb1;
+        //private ExThumb MyExThumb2;
+        //private ExThumb MyGroupExThumb1;
+        private int CountForName;
+        private ObservableCollection<ExThumb> MyLayers = new();
+        private ExThumb MyCurrentLayer;
+        private ExThumb MyMainPanel;
+        public ExThumb CurrentExThumb
+        {
+            get => currentExThumb; set
+            {
+                currentExThumb = value;
+                DataContext = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyInitialize();
+
+            ExThumb layer = MakeLayer("Layer1");
+            MyMainPanel.AddChildrenExThumb(layer);
+            MyLayers.Add(layer);
+            MyCurrentLayer = layer;
+            //Test1();
+            //GroupTest1();
+
+        }
+        private void MyInitialize()
+        {
+            //最上位Thumb、ここに全てを追加していくことになる
+            ExThumb panel = new(this, "MainPanel");
+            panel.RemoveDragDeltaEvent();
+            panel.IsLayer = true;
+            panel.Focusable = false;
+            MyGrid.Children.Add(panel);
+            //Grid.SetColumn(panel, 0);
+            MyMainPanel = panel;
+        }
+        private ExThumb MakeLayer(string name)
+        {
+            //レイヤー、最上位の直下のChildren
+            ExThumb layer = new(this, name);
+            layer.RemoveDragDeltaEvent();
+            layer.IsLayer = true;
+            layer.Focusable = false;
+            return layer;
+        }
+
+
+        //private void Test1()
+        //{
+        //    ExThumb exThumb1 = new(this, "textBlock1", MakeTextBloxk("textBlock1", 30, Brushes.Cyan), 50, 0);
+        //    ExThumb exThumb2 = new(this, "textBlock2", MakeTextBloxk("textBlock2", 30, Brushes.Cyan), 0, 100);
+        //    MyCanvas.Children.Add(exThumb1);
+        //    MyCanvas.Children.Add(exThumb2);
+        //    MyExThumb1 = exThumb1;
+        //    MyExThumb2 = exThumb2;
+        //}
+        //private void GroupTest1()
+        //{
+        //    TextBlock t1 = MakeTextBloxk("GroupItem1", 30, Brushes.Magenta);
+        //    ExThumb ex1 = new(this, "GroupItem1", t1, 0, 0);
+        //    TextBlock t2 = MakeTextBloxk("GroupItem2", 30, Brushes.MediumAquamarine);
+        //    ExThumb ex2 = new(this, "GroupItem2", t2, 100, 100);
+        //    ExThumb exGroup = new(this, "Group", 200, 100);
+        //    exGroup.AddChildrenExThumb(ex1);
+        //    exGroup.AddChildrenExThumb(ex2);
+        //    MyCanvas.Children.Add(exGroup);
+        //    MyGroupExThumb1 = exGroup;
+        //}
+        private TextBlock MakeTextBloxk(string text, double fontsize, Brush brush)
+        {
+            TextBlock textBlock = new();
+            textBlock.Text = text;
+            textBlock.FontSize = fontsize;
+            textBlock.Background = brush;
+            return textBlock;
+        }
+
+        private void ButtonTest1_Click(object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext;
+            var cex = CurrentExThumb;
+            var panel = MyMainPanel;
+            var layer = MyCurrentLayer;
+
+            var panelParent = MyMainPanel.ParentExThumb;
+            var layerParent = layer.ParentExThumb;
+
+        }
+
+        private void ButtonGroup_Click(object sender, RoutedEventArgs e)
+        {
+            ////1と2をグループ化
+            ////グループ化Thumbの座標決定
+            //double x = MyExThumb1.Left;
+            //if (x > MyExThumb2.Left) { x = MyExThumb2.Left; }
+            //double y = MyExThumb1.Top;
+            //if (y > MyExThumb2.Top) { y = MyExThumb2.Top; }
+            //ExThumb group = new(this, "Group1", x, y);
+            ////Itemの座標修正
+            //MyExThumb1.Left -= x; MyExThumb1.Top -= y;
+            //MyExThumb2.Left -= x; MyExThumb2.Top -= y;
+            ////ItemをCanvasから削除
+            //MyCanvas.Children.Remove(MyExThumb1);
+            //MyCanvas.Children.Remove(MyExThumb2);
+            ////ItemをグループThumbに追加
+            //group.AddChildrenExThumb(MyExThumb1);
+            //group.AddChildrenExThumb(MyExThumb2);
+            //MyCanvas.Children.Add(group);
+            //MyGroupExThumb1 = group;
+        }
+
+        private void ButtonUnGroup_Click(object sender, RoutedEventArgs e)
+        {
+            //double x = MyGroupExThumb1.Left;
+            //double y = MyGroupExThumb1.Top;
+            //foreach (var item in MyGroupExThumb1.Children)
+            //{
+            //    MyGroupExThumb1.RemoveChildrenExThumb(item);
+            //    item.Left += x;
+            //    item.Top += y;
+            //    MyCanvas.Children.Add(item);
+            //}
+            //MyCanvas.Children.Remove(MyGroupExThumb1);
+            //MyGroupExThumb1 = null;
+        }
+
+        private void ButtonAddThumb_Click(object sender, RoutedEventArgs e)
+        {
+            ExThumb ex = new(this, $"textBlock{CountForName}");
+            ex.AddChildrenElement(MakeTextBloxk($"Test{CountForName}", 30, Brushes.Magenta));
+            CountForName++;
+            MyCurrentLayer.AddChildrenExThumb(ex);
+        }
+    }
+}
