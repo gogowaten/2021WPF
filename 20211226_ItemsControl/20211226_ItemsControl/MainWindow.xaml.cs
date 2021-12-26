96983308cba1981a53de187787b017dbc60423a3
waten <gogowaten@yahoo.co.jp>
2021-12-26 15:34:23 +0900

    202112261534

---
+++ 20211226_ItemsControl/20211226_ItemsControl/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+//WPF ItemsControlをDataGridみたいに使う - No more Death March
+//https://nomoredeathmarch.hatenablog.com/entry/2019/01/21/003825
+
+namespace _20211226_ItemsControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Data> MyData { get; set; } = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Data d = new Data() { Left = i * 5, Top = i * 10, Name = $"test{i}" };                
+                MyData.Add(d);
+            }
+            DataContext = MyData;
+
+        }
+    }
+    public class Data
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public string Name { get; set; }
+    }
+
+}
